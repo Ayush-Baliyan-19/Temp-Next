fe4dc641260335023489d8de1905f2ebc93633be
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-11-23 17:47:03 +0530

    Screen3

---
+++ src/app/Screen3/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="bg-primary text-2xl text-secondary h-screen w-screen flex justify-center items-center">
+      Dashboard Generating, Please Wait...
+    </div>
+  );
+};
+
+export default Loading;
