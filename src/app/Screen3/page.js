57ba6794c061b0c8ea95c51a43e21d98b39fbe62
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-11-23 17:56:26 +0530

    Left Component added

--- src/app/Screen3/page.js
+++ src/app/Screen3/page.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react"
 // import Screen from "./components/center/center"
-// import LeftComponent from "./components/left/left"
+import LeftComponent from "./components/left/left"
 // import Right from "./components/right/right"
 import { BiSolidLeftArrow } from "react-icons/bi"
 
@@ -50,95 +50,91 @@ export default function Page() {
   }, [personalisedDBO])
 
   return (
-    <></>
-    // <>
-    //   <div className="flex justify-between bg-primary  items-center w-screen overflow-x-hidden">
-    //     {/* Left Component */}
-    //     <div
-    //       className={`leftSection bg-[#9F8EFF] relative z-10 h-screen w-min p-6 flex justify-start items-start flex-col transition-all ${
-    //         isLeftExpanded ? "transition-all" : "-translate-x-full transition-all"
-    //       }`}
-    //     >
-    //       <div
-    //         className="toggleLeft absolute right-[-2.45rem] bg-[#9F8EFF] p-2 mt-[-1.5rem] rounded-r-lg"
-    //         onClick={toggleLeft}
-    //       >
-    //         {isLeftExpanded ? (
-    //           <BiSolidLeftArrow className="text-white text-2xl" />
-    //         ) : (
-    //           <BiSolidLeftArrow className="text-white text-2xl rotate-180" />
-    //         )}
-    //       </div>
-    //       <LeftComponent dbo={db} clicked={clicked} setClicked={setClicked} />
-    //     </div>
-    //     {/* Center Component */}
-    //     <div className="midSection h-screen flex justify-center items-start">
-    //       <Screen
-    //         db={db}
-    //         setClicked={setClicked}
-    //         clicked={clicked}
-    //         userType={userType}
-    //         complexity={complexityType}
-    //         {...{
-    //           personalisedDBO,
-    //           setPersonalisedDBO,
-    //           selectedPersonalisedDBO,
-    //           isperosnalisedDBO,
-    //           unLockedPersonalisedDBo,
-    //           setUnLockedPersonalisedDBo,
-    //           lockedDBOs,
-    //           setLockedDBOs,
-    //           setIsLeftExpanded,
-    //           setIsRightExpanded,
-    //           isLeftExpanded,
-    //           isRightExpanded
-    //         }}
-    //         setUserType={setUserType}
-    //         complexityType={complexityType}
-    //         setComplexityType={setComplexityType}
-    //         layoutType={layoutType}
-    //         setLayoutType={setLayoutType}
-    //       />
-    //     </div>
-    //     {/* Right Component */}
-    //     <div
-    //       className={`rightSection bg-[#9F8EFF] h-screen p-6 flex flex-col justify-start items-start relative ${
-    //         isRightExpanded
-    //           ? " transition-all"
-    //           : "translate-x-full transition-all"
-    //       }`}
-    //     >
-    //       <div
-    //         className="toggleRight absolute left-[-2.45rem] bg-[#9F8EFF] p-2 mt-[-1.5rem] rounded-l-lg"
-    //         onClick={toggleRight}
-    //       >
-    //         {isRightExpanded ? (
-    //           <BiSolidLeftArrow className="text-white text-2xl transform rotate-180" />
-    //         ) : (
-    //           <BiSolidLeftArrow className="text-white text-2xl" />
-    //         )}
-    //       </div>
-    //       {/* <button className="self-start mb-4" onClick={toggleRight}>
-    //       Toggle Right
-    //     </button> */}
-    //       <Right
-    //         setDB={setDB}
-    //         db={db}
-    //         {...{
-    //           selectedPersonalisedDBO,
-    //           isperosnalisedDBO,
-    //           personalisedDBO,
-    //           setPersonalisedDBO,
-    //           setIsPersonalisedDBO,
-    //           setSelectedPersonalisedDBO,
-    //           unLockedPersonalisedDBo,
-    //           setUnLockedPersonalisedDBo,
-    //           lockedDBOs,
-    //           setLockedDBOs
-    //         }}
-    //       />
-    //     </div>
-    //   </div>
-    // </>
+    <>
+      <div className="flex justify-between bg-primary  items-center w-screen overflow-x-hidden">
+        {/* Left Component */}
+        <div
+          className={`leftSection bg-[#9F8EFF] relative z-10 h-screen w-min p-6 flex justify-start items-start flex-col transition-all ${
+            isLeftExpanded ? "transition-all" : "-translate-x-full transition-all"
+          }`}
+        >
+          <div
+            className="toggleLeft absolute right-[-2.45rem] bg-[#9F8EFF] p-2 mt-[-1.5rem] rounded-r-lg"
+            onClick={toggleLeft}
+          >
+            {isLeftExpanded ? (
+              <BiSolidLeftArrow className="text-white text-2xl" />
+            ) : (
+              <BiSolidLeftArrow className="text-white text-2xl rotate-180" />
+            )}
+          </div>
+          <LeftComponent dbo={db} clicked={clicked} setClicked={setClicked} />
+        </div>
+        {/* Center Component */}
+        {/* <div className="midSection h-screen flex justify-center items-start">
+          <Screen
+            db={db}
+            setClicked={setClicked}
+            clicked={clicked}
+            userType={userType}
+            complexity={complexityType}
+            {...{
+              personalisedDBO,
+              setPersonalisedDBO,
+              selectedPersonalisedDBO,
+              isperosnalisedDBO,
+              unLockedPersonalisedDBo,
+              setUnLockedPersonalisedDBo,
+              lockedDBOs,
+              setLockedDBOs,
+              setIsLeftExpanded,
+              setIsRightExpanded,
+              isLeftExpanded,
+              isRightExpanded
+            }}
+            setUserType={setUserType}
+            complexityType={complexityType}
+            setComplexityType={setComplexityType}
+            layoutType={layoutType}
+            setLayoutType={setLayoutType}
+          />
+        </div> */}
+        {/* Right Component */}
+        {/* <div
+          className={`rightSection bg-[#9F8EFF] h-screen p-6 flex flex-col justify-start items-start relative ${
+            isRightExpanded
+              ? " transition-all"
+              : "translate-x-full transition-all"
+          }`}
+        >
+          <div
+            className="toggleRight absolute left-[-2.45rem] bg-[#9F8EFF] p-2 mt-[-1.5rem] rounded-l-lg"
+            onClick={toggleRight}
+          >
+            {isRightExpanded ? (
+              <BiSolidLeftArrow className="text-white text-2xl transform rotate-180" />
+            ) : (
+              <BiSolidLeftArrow className="text-white text-2xl" />
+            )}
+          </div>
+          <Right
+            setDB={setDB}
+            db={db}
+            {...{
+              selectedPersonalisedDBO,
+              isperosnalisedDBO,
+              personalisedDBO,
+              setPersonalisedDBO,
+              setIsPersonalisedDBO,
+              setSelectedPersonalisedDBO,
+              unLockedPersonalisedDBo,
+              setUnLockedPersonalisedDBo,
+              lockedDBOs,
+              setLockedDBOs
+            }}
+          />
+        </div> */}
+      </div>
+    </>
   )
 }
