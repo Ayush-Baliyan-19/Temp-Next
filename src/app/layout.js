8cfdcb7e1673d314b476a3cbccb806437b926096
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-11-25 13:14:51 +0530

    inter removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,7 @@
 import StateProvider from './StateProvider'
 import Navbar from './components/Navbar/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { usePathname } from 'next/navigation'
-
-const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({ children }) {
@@ -17,7 +14,7 @@ export default function RootLayout({ children }) {
         <script src="https://cdn.jsdelivr.net/npm/vega-lite@5" async></script>
         <script src="https://cdn.jsdelivr.net/npm/vega-embed@6" async></script>
       </head> */}
-      <body className={inter.className}>
+      <body>
         <StateProvider>
           {
             pathName !== '/Screen3' &&
