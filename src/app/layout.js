1c2b4497ed48924e62d165d5f86ee7e25130b624
AyushbaliyanDOTS <ayush.baliyan@dotsforall.com>
2023-11-29 23:56:07 +0530

    idk

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,6 @@ export default function RootLayout({ children }) {
   const pathName = usePathname()
   return (
     <html lang="en">
-      {/* <head>
-        <script src="https://cdn.jsdelivr.net/npm/vega@5" async></script>
-        <script src="https://cdn.jsdelivr.net/npm/vega-lite@5" async></script>
-        <script src="https://cdn.jsdelivr.net/npm/vega-embed@6" async></script>
-      </head> */}
       <body>
         <StateProvider>
           {
